1975ec6d3994b66ab0db77503fca371e78a95d27
a-amato3 <aaronamato@hotmail.com>
2023-06-05 16:49:47 +0100

    Basic ngModel implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 import { HttpClient } from '@angular/common/http';
-import { CharacterData } from './types/characters';
+import { Character } from './types/character.model';
 import { tap } from 'rxjs';
 
 @Component({
@@ -10,26 +10,69 @@ import { tap } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  private publicKey = '42e4e75d5db25ebeb8f6f3433b1a6378';
-  private privateKey = '5ec2279b9a08f936eab47ea11af240f9dd079c20';
-  private timestamp = new Date().getTime().toString();
-  private apiUrl = 'https://gateway.marvel.com/v1/public/characters';
+  publicKey = '42e4e75d5db25ebeb8f6f3433b1a6378';
+  privateKey = '5ec2279b9a08f936eab47ea11af240f9dd079c20';
+  timestamp = new Date().getTime().toString();
+  apiUrl = 'https://gateway.marvel.com/v1/public/characters';
+  limit = 100;
+  offset = 0;
+  allCharacters: any[] = []; // Initialize allCharacters array
 
-  private hash: string = CryptoJS.MD5(
+  hash: string = CryptoJS.MD5(
     this.timestamp + this.privateKey + this.publicKey
   ).toString();
-  private url = `${this.apiUrl}?ts=${this.timestamp}&apikey=${this.publicKey}&hash=${this.hash}`;
+  url = `${this.apiUrl}?ts=${this.timestamp}&apikey=${this.publicKey}&hash=${this.hash}`;
+
+  filteredCharacters: any[] = [];
+  showSuggestions: boolean = false;
+  searchTerm: string = '';
 
   constructor(private readonly http: HttpClient) {}
 
   public ngOnInit(): void {
-    this.http
-      .get(this.url)
-      .pipe(
-        tap((characters: any) => {
-          console.log(characters.data.results);
-        })
-      )
-      .subscribe();
+    this.fetchCharacters();
+  }
+
+  private fetchCharacters(): void {
+    const fetchNextCharacters = (): void => {
+      const urlWithPagination = `${this.url}&limit=${this.limit}&offset=${this.offset}`;
+
+      this.http.get<any>(urlWithPagination).subscribe(
+        response => {
+          const characters = response.data.results;
+          this.allCharacters.push(...characters);
+
+          if (characters.length === this.limit) {
+            this.offset += this.limit;
+            fetchNextCharacters();
+          } else {
+            this.filteredCharacters = this.allCharacters;
+          }
+        },
+        error => {
+          console.error('An error occurred while fetching characters:', error);
+          // Handle the error as needed
+        }
+      );
+    };
+
+    fetchNextCharacters();
+  }
+
+  public onSearchChange(): void {
+    this.filteredCharacters = this.allCharacters.filter(
+      (character: Character) => {
+        return character.name
+          .toLowerCase()
+          .startsWith(this.searchTerm.toLowerCase());
+      }
+    );
+    this.showSuggestions = true;
+  }
+
+  public selectCharacter(character: any): void {
+    this.searchTerm = ''; // Clear the input field
+    this.showSuggestions = false;
+    alert(character.name); // Display an alert with the character's name
   }
 }
--- src/app/types/characters.ts
+++
@@ -1,7 +0,0 @@
-export interface CharacterData {
-  data: CharacterName;
-}
-
-interface CharacterName {
-  name: string;
-}
